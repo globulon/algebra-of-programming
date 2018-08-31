281a623c9f3cef6fb6d1d5ddb92764901c3a1f3d
Marc-Daniel Ortega <globulon@gmail.com>
2018-08-31 18:57:34 +0100

    [CONF] Update setting

--- project/TestDependencies.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-import scala.language.{higherKinds, implicitConversions, postfixOps}
-
-trait TestDependencies { self: Versions =>
-  final protected def scalaTest: ModuleID = "org.scalatest" %% "scalatest" % scalaTestVer % Test withSources()
-}
